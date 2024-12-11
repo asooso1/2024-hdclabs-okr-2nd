b5d03d799241f5a00123344df16647f9f7c139a3
leejs <leejs@hdc-labs.com>
2024-12-11 10:21:30 +0900

    style/update

--- src/components/Charts/ChartTwo.tsx
+++ src/components/Charts/ChartTwo.tsx
@@ -76,11 +76,11 @@ const ChartTwo: React.FC<ChartTwoProps> = ({ data }) => {
   };
 
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
+    <div className="col-span-12 rounded-lg border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
       <div className="mb-4 justify-between gap-4 sm:flex">
         <div>
           <h4 className="text-xl font-bold text-black dark:text-white">
-            작업 완료율
+            작업 현황
           </h4>
         </div>
       </div>
